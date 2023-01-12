ddffc759dc928bf66722e179c448fda0852784f6
Rodrigo Ramos <digojramos@gmail.com>
2023-01-12 09:45:32 -0300

    GET Clients paged, POST new client

--- src/main/java/com/rodrigojramos/crud/services/ClientService.java
+++ src/main/java/com/rodrigojramos/crud/services/ClientService.java
@@ -4,6 +4,8 @@ import com.rodrigojramos.crud.dto.ClientDTO;
 import com.rodrigojramos.crud.entities.Client;
 import com.rodrigojramos.crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +21,27 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 }
